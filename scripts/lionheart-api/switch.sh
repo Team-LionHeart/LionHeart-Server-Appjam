3906582d3f221e44af8db41cdf00488337a82976
HyeokJoon Kong <orijoon98@gmail.com>
2023-07-11 17:33:25 +0900

    🔧: chore API 서버 dev CI/CD 수정 #11

--- scripts/lionheart-api/switch.sh
+++ scripts/lionheart-api/switch.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
-echo "> 현재 구동중인 Port 확인"
-CURRENT_PORT=$(cat /etc/nginx/conf.d/service-url.inc | grep -Po '[0-9]+' | tail -1)
+NOW_TIME="$(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
 
-# 쉬고 있는 port 찾기: 8081이 사용중이면 8082가 쉬고 있고, 반대면 8081이 쉬고 있음
-if [ $CURRENT_PORT -eq 8081 ]
-then
-  IDLE_PORT=8082
-elif [ $CURRENT_PORT == 8082 ]
-then
-  IDLE_PORT=8081
+CURRENT_PORT=$(cat /etc/nginx/conf.d/service-url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+echo "[$NOW_TIME] Nginx currently proxies to ${CURRENT_PORT}." >> /home/ubuntu/api-server/deploy.log
+
+# Toggle port number
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+  TARGET_PORT=8081
 else
-  echo "> 일치하는 PORT가 없습니다."
-  echo "> 8081을 할당합니다."
-  IDLE_PORT=8081
+  echo "[$NOW_TIME] No WAS is connected to nginx" >> /home/ubuntu/api-server/deploy.log
+  exit 1
 fi
 
-echo "> 전환할 Port: $IDLE_PORT"
-echo "> Port 전환"
-echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" |sudo tee /etc/nginx/conf.d/service-url.inc
+# Change proxying port into target port
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
 
-echo "> Nginx Current Proxy Port: $IDLE_PORT"
-
-echo "> Nginx Reload"
+echo "[$NOW_TIME] Now Nginx proxies to ${TARGET_PORT}." >> /home/ubuntu/api-server/deploy.log
+# Reload nginx
 sudo service nginx reload
+
+echo "[$NOW_TIME] Nginx reloaded." >> /home/ubuntu/api-server/deploy.log
