e075889e3fa5d9e9b64c08509edb7003cb049df4
PgmJun <chltmdwns96@gmail.com>
2023-07-13 03:18:24 +0900

    🐛: bugfix JpaRepository 엔티티 종류 오류 수정 #16

--- lionheart-domain/src/main/java/com/chiwawa/lionheart/domain/domain/article/articleContent/repository/ArticleContentRepository.java
+++ lionheart-domain/src/main/java/com/chiwawa/lionheart/domain/domain/article/articleContent/repository/ArticleContentRepository.java
@@ -2,8 +2,8 @@ package com.chiwawa.lionheart.domain.domain.article.articleContent.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.chiwawa.lionheart.domain.domain.articlebookmark.ArticleBookmark;
+import com.chiwawa.lionheart.domain.domain.article.articleContent.ArticleContent;
 
-public interface ArticleContentRepository extends JpaRepository<ArticleBookmark, Long>,
+public interface ArticleContentRepository extends JpaRepository<ArticleContent, Long>,
 	ArticleContentRepositoryCustom {
 }
