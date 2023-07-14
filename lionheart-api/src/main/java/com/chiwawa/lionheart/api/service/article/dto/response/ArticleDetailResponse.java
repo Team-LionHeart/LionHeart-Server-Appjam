08a5c5bfd76845a67ed0dc3e783f97bdc3b22778
PgmJun <chltmdwns96@gmail.com>
2023-07-14 11:45:29 +0900

    🎨: style Swagger description 추가 #24

--- lionheart-api/src/main/java/com/chiwawa/lionheart/api/service/article/dto/response/ArticleDetailResponse.java
+++ lionheart-api/src/main/java/com/chiwawa/lionheart/api/service/article/dto/response/ArticleDetailResponse.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.chiwawa.lionheart.domain.domain.article.Article;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,11 +19,22 @@ import lombok.ToString;
 @Builder(access = AccessLevel.PRIVATE)
 public class ArticleDetailResponse {
 
+	@Schema(description = "제목")
 	private String title;
+
+	@Schema(description = "저자")
 	private String author;
+
+	@Schema(description = "메인 이미지 URL")
 	private String mainImageUrl;
+
+	@Schema(description = "메인 이미지 캡션")
 	private String mainImageCaption;
+
+	@Schema(description = "북마크 여부")
 	private Boolean isMarked;
+
+	@Schema(description = "내용")
 	private List<ArticleContentDto> contents;
 
 	public static ArticleDetailResponse of(Article article, boolean isMarked, List<ArticleContentDto> contents) {
